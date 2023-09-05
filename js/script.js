df3e4e1e598bd2640c0f4b24e32e14f5aca0db96
AdemHos <ademhos1907@outlook.com>
2023-09-06 00:22:45 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+const  errMsg =document.getElementById('err-msg');
+const formLink =document.getElementById('link-form');
+const input = document.getElementById('link-input');
+
+btn.addEventListener('click',navToggle)
+formLink.addEventListener('submit',formSubmit)
+
+// Validate a URL
+function validURL(str) {
+    var pattern = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    )
+    return !!pattern.test(str)
+  }
+
+function formSubmit (e) {
+  e.preventDefault()
+
+  if(input.value === '') {
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.add('border-red')
+  }else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid URL'
+    input.classList.add('border-red')
+  }else{
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.remove('border-red')
+  }
+}
+
+// Mobile Menu Toggle
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
+
